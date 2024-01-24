0cd2cc421ae4f7292d0a9b62d9ad2c673c26cb48
sekars3 <sekarrakesh123@gmail.com>
2024-01-24 23:14:39 +0530

    modified code

--- Python/python_test.py
+++ Python/python_test.py
@@ -14,10 +14,3 @@ def reverse_num(num):
         temp_num = temp_num//10
         reminder = temp_num%10
     return rev_num
-    
-def reverse_string_new(word):
-    rev_word = ""
-    len_word = len(word)
-    for i in range(0,len_word):
-        rev_word += word[len_word-1-i]
-    return rev_word
