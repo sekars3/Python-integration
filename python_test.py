7d60051419897d07a3182fc84000002672328522
sekars3 <sekarrakesh123@gmail.com>
2024-02-15 01:08:44 +0530

    yaml test

--- python_test.py
+++ python_test.py
@@ -1,3 +1,12 @@
 import sys
  
 print("printing input parameter value :", sys.argv[1])
+
+#open text file
+text_file = open("in_plan.yaml", "w")
+#write string to file
+n = text_file.write(sys.argv[1])
+#close file
+text_file.close()
+ 
+print(n)
