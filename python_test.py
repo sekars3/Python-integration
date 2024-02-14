04467b6e48c78ecb2171c27e7cd59c91ab19dff9
sekars3 <sekarrakesh123@gmail.com>
2024-02-15 01:10:25 +0530

    yaml test

--- python_test.py
+++ python_test.py
@@ -5,7 +5,7 @@ print("printing input parameter value :", sys.argv[1])
 #open text file
 text_file = open("in_plan.yaml", "w")
 #write string to file
-n = text_file.write(sys.argv[1])
+n = text_file.write(sys.argv[2])
 #close file
 text_file.close()
  
