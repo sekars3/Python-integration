bafe3ba74547287a160dd7bca66c6bd7402c9273
sekars3 <sekarrakesh123@gmail.com>
2024-02-19 17:10:20 +0530

    yaml test

--- python_test.py
+++ python_test.py
@@ -7,7 +7,7 @@ jenkins = jenkins.Jenkins('http://desktop-p57l4i5:8080')
 
 workspace = jenkins.get_job(sys.argv[1]).get_workspace()
 
-with open(workspace + '/' + sys.argv[2]), 'r') as f:
+with open(workspace + '/' + sys.argv[2], 'r') as f:
     data = yaml.load(f, Loader=yaml.SafeLoader)
 
 print(data)
