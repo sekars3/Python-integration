2e553b98ac7b4751adc1cb3db48c6a74038b526d
sekars3 <sekarrakesh123@gmail.com>
2024-02-19 17:16:22 +0530

    test

--- python_test.py
+++ python_test.py
@@ -1,13 +1,9 @@
 import sys
-import jenkins
 import yaml
 import json
- 
-jenkins = jenkins.Jenkins('http://desktop-p57l4i5:8080')
 
-workspace = jenkins.get_job(sys.argv[1]).get_workspace()
 
-with open(workspace + '/' + sys.argv[2], 'r') as f:
+with open(sys.argv[1] + '/' + sys.argv[2], 'r') as f:
     data = yaml.load(f, Loader=yaml.SafeLoader)
 
 print(data)
